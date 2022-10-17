6db5710d24cc3d2652211aa6788d08d2fafd2498
victoriaDunlap <victoriacdunlap@gmail.com>
2022-10-17 13:24:42 -0400

    semantic ui integrated in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,6 @@
 function NavBar(props) {
     return (
-        <div className="ui secondary  menu">
-            <div className="sidenav" style={{ width: '25%', paddingTop: '20px' }}>
+            <div className="sidenav ui secondary pointing menu" style={{ width: '25%', paddingTop: '20px' }}>
                 <a className="active item" href="#section">Home</a>
                 <a className="item" href="#section">Our Story</a>
                 <a className="item" href="#section">When & Where</a>
@@ -10,7 +9,6 @@ function NavBar(props) {
                 <a className="item" href="#section">Gallery</a>
                 <a className="item" href="#section">RSVP</a>  
             </div>
-        </div>
     )
 }
 
