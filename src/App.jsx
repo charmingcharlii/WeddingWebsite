e412931a92c91441dc60aab3f3afed552218d06b
victoriaDunlap <victoriacdunlap@gmail.com>
2022-10-17 12:57:58 -0400

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <p>
-      Cleanedup React App
-    </p>
+   <>
+    <NavBar />
+   </>
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+function NavBar(props) {
+    return (
+        <div className="sidenav" style={{ width: '25%', paddingTop: '20px' }}>
+            <a href="#section">Home</a>
+            <a href="#section">Our Story</a>
+            <a href="#section">When & Where</a>
+            <a href="#section">Registry</a>
+            <a href="#section">Bridal Party</a>  
+            <a href="#section">Gallery</a>
+            <a href="#section">RSVP</a>  
+        </div>
+    )
+}
+
+export default NavBar
