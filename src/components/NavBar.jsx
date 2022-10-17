701debccea5b3e315d0f803fbeea525311a68b1d
victoriaDunlap <victoriacdunlap@gmail.com>
2022-10-17 13:12:13 -0400

    testing semantic ui menu styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,15 @@
 function NavBar(props) {
     return (
-        <div className="sidenav" style={{ width: '25%', paddingTop: '20px' }}>
-            <a href="#section">Home</a>
-            <a href="#section">Our Story</a>
-            <a href="#section">When & Where</a>
-            <a href="#section">Registry</a>
-            <a href="#section">Bridal Party</a>  
-            <a href="#section">Gallery</a>
-            <a href="#section">RSVP</a>  
+        <div className="ui secondary  menu">
+            <div className="sidenav" style={{ width: '25%', paddingTop: '20px' }}>
+                <a className="active item" href="#section">Home</a>
+                <a className="item" href="#section">Our Story</a>
+                <a className="item" href="#section">When & Where</a>
+                <a className="item" href="#section">Registry</a>
+                <a className="item" href="#section">Bridal Party</a>  
+                <a className="item" href="#section">Gallery</a>
+                <a className="item" href="#section">RSVP</a>  
+            </div>
         </div>
     )
 }
