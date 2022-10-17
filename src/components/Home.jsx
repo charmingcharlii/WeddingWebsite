8252622313fd76e8131d91a7f65289e94bc0c2e2
victoriaDunlap <victoriacdunlap@gmail.com>
2022-10-17 15:25:21 -0400

    rsvp button added to home screen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 function Home(props) {
-    const myStyle={
+    const coverImageStyle={
         position: 'relative',
         backgroundImage: "url('/engagmentMtn.jpg')",
         height:'100vh',
@@ -9,8 +9,14 @@ function Home(props) {
         backgroundRepeat: 'no-repeat',
     };
     return (
-        <div style = {myStyle}>
+        <div style = {coverImageStyle}>
             <p className = "welcomeMessage">Some kind of welcome</p>
+            <div className="ui animated button green rsvpButton" tabindex="0">
+            <div className="visible content">RSVP</div>
+            <div className="hidden content">
+                <i className="right arrow icon"></i>
+            </div>
+            </div>
         </div>
     )
 }
