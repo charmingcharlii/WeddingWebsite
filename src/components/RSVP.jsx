877ad2feca8b52515220c2735e7b7e2b1d2776ba
victoriaDunlap <victoriacdunlap@gmail.com>
2022-10-27 11:07:53 -0400

    adjusting rsvp form

--- src/components/RSVP.jsx
+++ src/components/RSVP.jsx
@@ -3,10 +3,10 @@ import {
   Button,
   Form,
   Input,
-  Radio,
+  // Radio,
   Select,
   TextArea,
-  Segment
+  // Segment
 } from 'semantic-ui-react'
 
 const options = [
@@ -31,11 +31,11 @@ class RSVP extends Component {
     // const { value } = this.state
     return (
     <div>
-        <h1 className='rsvpHeading'>RSVP</h1>
-    <Segment inverted className='rsvpForm'>
-      <Form inverted id='rsvp'>
+      {/* ui loading form */}
+      <Form id='rsvp' className='rsvpForm '>
+      <h1 className='rsvpHeading'>RSVP</h1>
         {/* Name input */}
-        <Form.Group widths='equal'>
+        <Form.Group>
           <Form.Field
             control={Input}
             label='First name (As appears on invitation)'
@@ -46,135 +46,25 @@ class RSVP extends Component {
             label='Last name (As appears on invitation)'
             placeholder='Last name'
           />
-          <Form.Field
+        </Form.Group>
+        <Form.Group>
+        <Form.Field
+          className='twelve wide'
             control={Select}
             label='Attending'
             options={options}
             placeholder='Y/N'
           />
         </Form.Group>
-        {/* How many adults */}
-        <Form.Group inline>
-          <label>Including yourself, how many adults in your party?</label>
-          <Form.Field
-            control={Radio}
-            label='One'
-            value='1'
-            checked={this.state.adultValue === '1'}
-            onChange={(e, data) => this.handleAdultChange(e, data)}
-          />
-          <Form.Field
-            control={Radio}
-            label='Two'
-            value='2'
-            checked={this.state.adultValue === '2'}
-            onChange={this.handleAdultChange}
-          />
-          <Form.Field
-            control={Radio}
-            label='Three'
-            value='3'
-            checked={this.state.adultValue === '3'}
-            onChange={this.handleAdultChange}
-          />
-          </Form.Group>
-          {/* name of extra adults */}
-          {this.state.adultValue === '2' ? 
-            <Form.Group>
-              <Form.Field
-                control={Input}
-                label='First name (As appears on invitation)'
-                placeholder='First name'
-              />
-              <Form.Field
-                control={Input}
-                label='Last name (As appears on invitation)'
-                placeholder='Last name'
-              />
-            </Form.Group> 
-            : 
-            <>
-            </>
-          } 
-          {this.state.adultValue === '3' ?
-              <Form.Group>
-                <Form.Field
-                  control={Input}
-                  label='First name (As appears on invitation)'
-                  placeholder='First name'
-                />
-                <Form.Field
-                  control={Input}
-                  label='Last name (As appears on invitation)'
-                  placeholder='Last name'
-                />
-                <Form.Field
-                  control={Input}
-                  label='First name (As appears on invitation)'
-                  placeholder='First name'
-                />
-                <Form.Field
-                  control={Input}
-                  label='Last name (As appears on invitation)'
-                  placeholder='Last name'
-                />
-              </Form.Group>
-             :
-             <>
-             </>
-          }
-
-          {/* How many kids */}
-          <Form.Group inline>
-           <label>How many kids in your party?</label>
-          <Form.Field
-            control={Radio}
-            label='Zero'
-            value='0'
-            checked={this.state.kidValue === '0'}
-            onChange={this.handleKidChange}
-          />
-          <Form.Field
-            control={Radio}
-            label='One'
-            value= '1'
-            checked={this.state.kidValue === '1'}
-            onChange={this.handleKidChange}
-          />
-          <Form.Field
-            control={Radio}
-            label='Two'
-            value='2'
-            checked={this.state.kidValue === '2'}
-            onChange={this.handleKidChange}
-          />
-          <Form.Field
-            control={Radio}
-            label='More'
-            value='3'
-            checked={this.state.kidValue === '3'}
-            onChange={this.handleKidChange}
-          />
-        </Form.Group>
-        {this.state.kidValue === '3' ?
-              <Form>
-                <Form.Field inline
-                control={Input}>
-                  <label>Total kids in your party</label>
-                  <Input placeholder='Number' />
-                </Form.Field>
-              </Form>
-             :
-             <>
-             </>
-          }
         {/* Extra info */}
+        <Form.Group>
         <Form.Field
           control={TextArea}
           label='Anything else the Bride and Groom need to know to accomodate your party?'
           placeholder='Tell us more...'
         />
-        <Form.Group widths='equal'>
+        </Form.Group>
+        <Form.Group>
           <Form.Field
             control={Input}
             label='Song Request #1 (Title and Artist)'
@@ -188,7 +78,7 @@ class RSVP extends Component {
         </Form.Group>
         <Form.Field control={Button}>Submit</Form.Field>
       </Form>
-      </Segment>
+  
       </div>
     )
   }
